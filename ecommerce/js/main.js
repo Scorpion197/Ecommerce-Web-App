91a38c991b4618a6794fc56465215f0a27426981
Scorpion197 <doudou.gaw@gmail.com>
2021-01-03 23:29:45 +0100

    More styling of the popular items cards

---
+++ ecommerce/js/main.js
@@ -0,0 +1,18 @@
+var images = document.querySelectorAll("#popular-items .row .card img ");
+
+// Display the "Add to cart caption" on hover 
+
+for (var i = 0 ; i < images.length; i++) {
+
+    images[i].addEventListener("mouseover", function() {
+
+        var captions = document.querySelectorAll("#popular-items .row .card .image-caption"); 
+
+        
+
+        /*this.style.border = "none"; 
+        this.style.outline = "none";*/
+    })
+}
+
+
