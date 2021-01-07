cebdd8d4b8749081ed20773df55cf5f547c07f33
Scorpion197 <doudou.gaw@gmail.com>
2021-01-07 21:36:09 +0100

    Fixed some responsiveness issues

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var input = document.querySelector("input"); 
+var logButton = document.getElementById("log-in");
+
+logButton.addEventListener("click", function(event) {
+
+	'use strict'; 
+	console.log(input.value);
+
+});
+
---
+++ rootme/main.js
@@ -0,0 +1,7 @@
+var url = document.getElementsByClassName("root-me"); 
+
+url.onclick = function (){
+    'use strict';
+    alert(this.hash);
+
+}
