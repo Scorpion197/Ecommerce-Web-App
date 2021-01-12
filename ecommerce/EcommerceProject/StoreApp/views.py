984c83383114c128cd9ffb2d7fc67acd2ee9cf83
Scorpion197 <doudou.gaw@gmail.com>
2021-01-12 21:54:48 +0100

    Added home page to the server

--- ecommerce/EcommerceProject/StoreApp/views.py
+++ ecommerce/EcommerceProject/StoreApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import request, Http404, HttpResponse, HttpResponseRedirect
+from .models import * 
+from django.utils import timezone 
+from django.core.mail import BadHeaderError, send_mail
 
 # Create your views here.
+def home_page(request):
+
+    return render(request, "home.html")
+
+    
