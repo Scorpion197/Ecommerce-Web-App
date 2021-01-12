40bd0dca0ed89c31673c2f1ea8a7e560e0e33411
Scorpion197 <doudou.gaw@gmail.com>
2021-01-12 21:32:58 +0100

    Finished linking PostgreSQL with django

--- ecommerce/EcommerceProject/EcommerceProject/settings.py
+++ ecommerce/EcommerceProject/EcommerceProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'EcommerceProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'ecommerce',
+        'PASSWORD': 'Ecommerce@201/', 
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
