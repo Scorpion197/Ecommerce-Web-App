84451265f59514d1f447d374790dbf0422cc1072
Scorpion197 <doudou.gaw@gmail.com>
2021-01-15 02:54:59 +0100

    Added single item view

--- ecommerce/EcommerceProject/StoreApp/views.py
+++ ecommerce/EcommerceProject/StoreApp/views.py
@@ -3,6 +3,8 @@ from django.http import request, Http404, HttpResponse, HttpResponseRedirect
 from .models import * 
 from django.utils import timezone 
 from django.core.mail import BadHeaderError, send_mail
+from django.contrib.auth import authenticate, login, logout
+ 
 
 # Create your views here.
 def home_page(request):
@@ -10,3 +12,7 @@ def home_page(request):
     return render(request, "home.html")
 
     
+def view_single_item(request, item_id):
+
+    pass 
+
