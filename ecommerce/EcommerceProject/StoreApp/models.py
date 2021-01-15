1f0a706d6a864b2cbeef468101fe8347c7bdbcbb
Scorpion197 <doudou.gaw@gmail.com>
2021-01-15 02:52:35 +0100

    Added watch Model

--- ecommerce/EcommerceProject/StoreApp/models.py
+++ ecommerce/EcommerceProject/StoreApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Watch(models.Model):
+
+    watch_name = models.CharField(max_length=50)
+    watch_price = models.IntegerField(default=0)
+    left_items = models.IntegerField(default=0)
+    image = models.ImageField()
+
+    
