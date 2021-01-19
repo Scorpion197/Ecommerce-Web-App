a4e9783790ed64d3941db114a54b42aaf36509df
Scorpion197 <doudou.gaw@gmail.com>
2021-01-19 11:11:12 +0100

    view single item functionnality added

--- ecommerce/EcommerceProject/StoreApp/urls.py
+++ ecommerce/EcommerceProject/StoreApp/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_page, name="home_page"),
+    path('store/<int:item_id>', views.view_single_item, name="view_watch"),
 ]
 
