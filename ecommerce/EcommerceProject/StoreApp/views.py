03da46dd650829d33472aa27d8c3814eb2e558b9
Scorpion197 <doudou.gaw@gmail.com>
2021-01-19 11:09:30 +0100

    Updated the single item view

--- ecommerce/EcommerceProject/StoreApp/views.py
+++ ecommerce/EcommerceProject/StoreApp/views.py
@@ -9,10 +9,26 @@ from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 def home_page(request):
 
-    return render(request, "home.html")
+    items = Watch.objects.all()[:3]
+    context = {"items": items}
+
+    return render(request, "home.html", context)
 
     
 def view_single_item(request, item_id):
 
-    pass 
+    """
+    A function to view a single product when we click on it
+    """
+
+    try: 
+
+        item = Watch.objects.get(id=item_id)
+        context = {"item":item}
+
+    except Watch.DoesNotExist:
+
+        raise Http404
+
+    return render(request, "singleitem.html", context)
 
